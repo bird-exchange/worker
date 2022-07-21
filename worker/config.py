8d2774a7815622af3967644cfe58d1b10e833445
ProtKsen <protcenko.kseniya@gmail.com>
2022-07-21 22:31:23 +0500

    feature: update config add file storage

--- worker/config.py
+++ worker/config.py
@@ -5,11 +5,13 @@ from pydantic import BaseModel
 
 class AppConfig(BaseModel):
     endpoint: str
+    temp_file_storage: str
 
 
-def load_from_env():
+def load_from_env() -> AppConfig:
     endpoint = os.environ['ENDPOINT']
-    return AppConfig(endpoint=endpoint)
+    temp_file_storage = os.environ['TEMP_FILE_STORAGE']
+    return AppConfig(endpoint=endpoint, temp_file_storage=temp_file_storage)
 
 
 config = load_from_env()
