8dc8a4a1c418e20b4ed7555913745e5d3181db58
ProtKsen <protcenko.kseniya@gmail.com>
2022-07-21 22:32:43 +0500

    feature: draft hadler return result file path

--- worker/handler/watcher.py
+++ worker/handler/watcher.py
@@ -1,2 +1,13 @@
-def draft_handler(file):
-    return file
+import os
+import pathlib
+
+from worker.config import config
+
+
+def draft_handler(file_path: str, filename: str) -> str:
+    result_file_path = f'{config.temp_file_storage}/res/{filename}'
+    pathlib.Path(f'{config.temp_file_storage}/res').mkdir(parents=True, exist_ok=True)
+
+    os.system(f'cp {file_path} {result_file_path}')
+
+    return result_file_path
