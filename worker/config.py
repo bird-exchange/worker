a2dfb9d72a466caf21f38e7d08f3a1fdde5052da
ProtKsen <protcenko.kseniya@gmail.com>
2022-07-22 20:51:21 +0500

    env: create config for handle

--- worker/config.py
+++ worker/config.py
@@ -8,6 +8,15 @@ class AppConfig(BaseModel):
     temp_file_storage: str
 
 
+class HandlerConfig(BaseModel):
+    path_to_data: str
+    path_load_model: str
+    path_test_results: str
+    image_size: int
+    load_epoch: int
+    switch: bool
+
+
 def load_from_env() -> AppConfig:
     endpoint = os.environ['ENDPOINT']
     temp_file_storage = os.environ['TEMP_FILE_STORAGE']
@@ -15,3 +24,12 @@ def load_from_env() -> AppConfig:
 
 
 config = load_from_env()
+
+handler_config = HandlerConfig(
+    path_to_data='{}/testA/'.format(os.environ['TEMP_FILE_STORAGE']),
+    path_load_model="worker/handler/checkpoints",
+    path_test_results='{}/res/'.format(os.environ['TEMP_FILE_STORAGE']),
+    image_size=256,
+    load_epoch=300,
+    switch=True
+)
